0d0a792e1be194a874fded7b004fb681232a68a7
Ahmed Athar <ahmedathar123@outlook.com>
2023-01-17 21:56:04 +0400

    Created a Game4u App

--- app/src/main/java/com/example/game4u/MainActivity.kt
+++ app/src/main/java/com/example/game4u/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.game4u
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        popup.setOnClickListener {
+            popup.visibility = View.INVISIBLE
+        }
+        var checker = intent.getIntExtra("checker", 0)
+        if (checker == 1) {
+            popup.visibility = View.INVISIBLE
+        }
 
         log_button.setOnClickListener {
             val intent= Intent(this, MainActivity2::class.java)
